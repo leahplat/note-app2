0162de006b66825279b6193908717b0c7001fd5b
Leah Plat <leahplat@gmail.com>
2019-08-19 12:01:57 -0400

    tag button

--- src/components/Note.js
+++ src/components/Note.js
@@ -10,31 +10,55 @@ class Note extends React.Component {
     };
     this.props.submitNote(formData, this.props.note.id);
   }
- 
-  render() {
-    const { note } = this.props;
 
+renderTagForm() {
+  if (!this.props.newTag) {
     return (
-      <div className="note-container">
-        <form className="note-form" onSubmit={(e) => this.onSubmit(e)}>
-          <input
-            className="note-title-input"
-            type="text"
-            placeholder="Note Title..."
-            defaultValue={note.title}
-            ref={(input) => this.title = input}
-           />
-          <textarea
-            className="note-textarea"
-            placeholder="Type Here..."
-            defaultValue={note.content}
-            ref={(input) => this.content = input}
-          />
-          <input className="note-button" type="submit" value="Submit" />
-        </form>
-      </div>
+      <span>
+        Tag your note:
+        <i
+          className="tag-button material-icons"
+          onClick={() => this.props.showTagForm()}
+        >
+            add circle
+        </i>
+      </span>
     );
   }
 }
+ 
+
+  render() {
+  const { note } = this.props;
+
+  return (
+    <div className="note-container">
+      <form className="note-form" onSubmit={(e) => this.onSubmit(e)}>
+        <input
+          className="note-title-input"
+          type="text"
+          placeholder="Note Title..."
+          defaultValue={note.title}
+          ref={(input) => this.title = input}
+         />
+        <textarea
+          className="note-textarea"
+          placeholder="Type Here..."
+          defaultValue={note.content}
+          ref={(input) => this.content = input}
+        />
+        <input className="note-button" type="submit" value="Submit" />
+      </form>
+      <div className="tag-container">
+        <div className="tag-button-container">
+          {this.renderTagForm()}
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+}
 
 export default Note;
+ 
