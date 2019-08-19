2486ee8134934fa278c5d5a5c4ddef18c9da0ba0
Leah Plat <leahplat@gmail.com>
2019-08-19 10:33:52 -0400

    lesson 14

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
 import axios from 'axios';
+import urlFor from './helpers/urlFor';
+
 
 class App extends Component {
   constructor() {
@@ -19,12 +21,12 @@ class App extends Component {
     });
   }
 
-  getNotes = () => {
-    axios.get('https://firehose-note-api.herokuapp.com/notes')
-    .then((res) => console.log(res.data) )
-    .catch((err) => console.log(err.repsonse.data) );
-  }
-
+getNotes = () => {
+  axios.get(urlFor('notes'))
+  .then((res) => console.log(res.data) )
+  .catch((err) => console.log(err.response.data) );
+}
+ 
   render() {
     const { showNote } = this.state;
 
